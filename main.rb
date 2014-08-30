f56ab6b728afc36bc17419702199694a46952aa6
ikaruga <ikaruga777@gmail.com>
2014-08-30 13:26:41 +0900

    にっこにっこにーできるようにした

--- main.rb
+++ main.rb
@@ -4,28 +4,15 @@
 require "rubygems"
 require "sinatra"
 require 'sinatra/reloader'
-require './SDVXScore'
+
 
 
 get '/' do
-v= ""
-s= "にっこにっこにー"
-while(v.slice(v.size - s.size, v.size) != s)
-	v += ("にっこにっこにー")[rand(s.size)]
-end
-	puts(v)
-printf("%d回目でおわり",v.size())end
-
-get '/:id' do
-	users = Array.new()
-	users.push params[:id]
-	users.push 'fm'
-
-	playdata = SDVXScore.new(users[0])
-	p playdata.music_scores
-	out = ""
-	out << playdata.music_scores.each{
-		|track|p track["title"].to_s
-	}.class
-	out
+	v= ""
+	s= "にっこにっこにー"
+	while(v.slice(v.size - s.size, v.size) != s)
+		v += ("にっこにっこにー")[rand(s.size)]
+	end
+	
+	return v + v.size.to_s+"回目でおわり"
 end
